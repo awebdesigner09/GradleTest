07da0ae398d5d8de8cf041ee66e202b9abf5f199
Naga Sai Katakamsetty <awebdesigner09@gmail.com>
2017-04-16 06:49:22 +0530

    init

---
+++ GradleTest/Tester/Tester/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.Read();
+        }
+    }
+}
